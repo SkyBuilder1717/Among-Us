8af0700504e05dd0ff43f7f1add3bda739186b70
SkyBuilder1717 <skybuilderoffical@yandex.ru>
2025-06-01 21:50:17 +0300

    seconds

--- mods/settings/button.lua
+++ mods/settings/button.lua
@@ -1,3 +1,5 @@
+local modname = core.get_current_modname()
+local S = core.get_translator(modname)
 local FORMNAME = "settings:button"
 
 function settings.show_button_menu(name)
@@ -17,7 +19,7 @@ core.register_on_player_receive_fields(function(player, formname, fields)
     local name = player:get_player_name()
     if fields.button then
         if settings.button_pressed then
-            core.chat_send_player(name, "Cooldown for 30 seconds after meeting!")
+            core.chat_send_player(name, S("Cooldown for @1 seconds after meeting!", settings.get_setting("button_delay")))
             return
         end
         settings.emergency_meeting(name)
