468801b33b822cf222c15d23f730030b7ee91317
SkyBuilder1717 <skybuilderoffical@yandex.ru>
2025-06-03 15:15:38 +0300

    fix

--- mods/settings/costumes.lua
+++ mods/settings/costumes.lua
@@ -22,13 +22,13 @@ function settings.show_costumes_menu(player_name)
         insert(formspec, "model[0,2;5,5;preview;")
         insert(formspec, props.mesh)
         insert(formspec, ";")
-        insert(formspec, core.formspec_escape(player_api.get_textures(player)))
+        insert(formspec, core.formspec_escape(table.concat(player_api.get_textures(player), ",")))
         insert(formspec, ";0,180;false;true;0,2.66;1]")
     else
         insert(formspec, "model[0,2;5,5;preview;")
         insert(formspec, props.mesh)
         insert(formspec, ";")
-        insert(formspec, core.formspec_escape(player_api.get_textures(player)))
+        insert(formspec, core.formspec_escape(table.concat(player_api.get_textures(player), ",")))
         insert(formspec, ";0,180;false;true;0,79;30]")
     end
     
